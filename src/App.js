05cfaa36bcd17dc1bd0c2260fa7a5744b5760bf0
Rei Lin <temisrei@gmail.com>
2021-01-17 19:35:15 +0800

    切版使用 Styled Components

--- src/App.js
+++ src/App.js
@@ -18,11 +18,78 @@ const WeatherCard = styled.div`
   padding: 30px 15px;
 `;
 
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  display: flex;  
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+`;
+
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  display: inline-flex;
+  align-items: flex-end;
+  font-size: 12px;
+  color: #828282;
+`;
+
 function App() {
   return (
     <Container>
       <WeatherCard>
-        <h1>Weather</h1>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>°C</Celsius>
+          </Temperature>
+        </CurrentWeather>
+        <AirFlow> 23 m/h </AirFlow>
+        <Rain> 48% </Rain>
+        <Refresh> 最後觀測時間：上午 12:03 </Refresh>
       </WeatherCard>
     </Container>
   );
