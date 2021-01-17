839c77321b8aab9d29a7fcbbc4a8612efb175e61
Rei Lin <temisrei@gmail.com>
2021-01-17 18:54:05 +0800

    改用 emotion 的 Styled Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
-import './App.css';
+// import './App.css';
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
 
 function App() {
   return (
-    <div className="container">
-      <div className="weather-card">
+    <Container>
+      <WeatherCard>
         <h1>Weather</h1>
-      </div>
-    </div>
+      </WeatherCard>
+    </Container>
   );
 }
 
