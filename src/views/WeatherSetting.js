9dfcbd4d29ff7ca8beb635016220d5c3ba31015c
Rei Lin <temisrei@gmail.com>
2021-01-23 12:38:43 +0800

    取得使用者想要儲存的地區資訊

--- src/views/WeatherSetting.js
+++ src/views/WeatherSetting.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from '@emotion/styled';
 import { availableLocations } from "./../utils/helpers";
 
@@ -94,18 +94,29 @@ const Save = styled.button`
 `;
 
 const WeatherSetting = ({ handleCurrentPageChage }) => {
+  const [locationName, setLocationName] = useState('高雄市');
+
+  const handleChange = (e) => {
+    setLocationName(e.target.value);
+  };
+
+  const handleSave = () => {
+    console.log(locationName);
+  }
+
   return (
     <WeatherSettingWrapper>
       <Title>設定</Title>
       <StyledLabel htmlFor="location">地區</StyledLabel>
-      <StyledSelect id="location" name="location">
+      <StyledSelect id="location" name="location" 
+        onChange={handleChange} value={locationName}>
         {availableLocations.map(({ cityName }) => (
           <option value={cityName} key={cityName}>{cityName}</option>
         ))}
       </StyledSelect>
       <ButtonGroup>
         <Back onClick={() => handleCurrentPageChage('WeatherCard')}>返回</Back>
-        <Save>儲存</Save>
+        <Save onClick={handleSave}>儲存</Save>
       </ButtonGroup>
     </WeatherSettingWrapper>
   );
